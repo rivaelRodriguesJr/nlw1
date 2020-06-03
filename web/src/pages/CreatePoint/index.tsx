0c0210b6d0270ccb6ddc928c1acd1403d9049bd4
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2020-06-03 16:47:05 -0300

    nlw dia 03 - adicionando rotas

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+
+const CreatePoint = () => {
+  return (
+    <div id="page-create-point">
+      <header>
+      </header>
+    </div>
+  );
+}
+
+export default CreatePoint;
